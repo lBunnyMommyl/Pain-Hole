395fe30115d12161a1a9cd61d21705c31e81a472
lBunnyMommyl <super.pan-10@ya.ru>
2025-09-20 11:16:40 +0000

    test3

--- 1/script.js
+++ 1/script.js
@@ -1,4 +1,4 @@
 const num1 = Math.floor(Math.random() * 100);
 const num2 = Math.floor(Math.random() * 100);
 const sum = num1 + num2;
-console.log(chalk.blue(`\x1b[33mСумма ${num1} + ${num2} = ${sum}\x1b[0m`));
+console.log(chalk.yellow(`Сумма ${num1} + ${num2} = ${sum}`));
