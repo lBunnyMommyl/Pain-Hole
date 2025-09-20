fb0d1d2c813e4a5b82764235ba3e5db1e119deac
lBunnyMommyl <super.pan-10@ya.ru>
2025-09-20 11:22:33 +0000

    test please

--- 1/script.js
+++ 1/script.js
@@ -2,4 +2,5 @@ const num1 = Math.floor(Math.random() * 100);
 const num2 = Math.floor(Math.random() * 100);
 const sum = num1 + num2;
 
+import chalk from 'chalk';
 console.log(chalk.yellow(`Сумма ${num1} + ${num2} = ${sum}`));
